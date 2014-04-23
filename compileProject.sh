a414d13b3addc6788b2399ff3b8ba45a3836079c
Gabor Berend <berendg@inf.u-szeged.hu>
2014-04-23 16:50:53 +0200

    includes scripts for compilation and running the project

---
+++ compileProject.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+javac -d bin -sourcepath src -cp "lib/*" src/hu/u_szeged/kpe/KpeMain.java
---
+++ runKpe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -Xmx3g -cp "bin:lib/*" hu.u-szeged.kpe.KpeMain -paramFile config.txt
