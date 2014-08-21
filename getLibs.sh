c89656addc52cca09663291ac58bfcd86c4184f0
Gabor Berend <berendg@inf.u-szeged.hu>
2014-08-21 02:07:35 +0200

    updates the getLib script

--- getLibs.sh
+++ getLibs.sh
@@ -7,4 +7,4 @@ unzip stanford-corenlp-full-2014-06-16.zip -d lib/
 wget https://google-gson.googlecode.com/files/google-gson-2.2.4-release.zip
 unzip google-gson-2.2.4-release.zip -d lib/
 
-rm stanford-corenlp-full-2014-01-04.zip  google-gson-2.2.4-release.zip
+rm stanford-corenlp-full-2014-06-16.zip  google-gson-2.2.4-release.zip
